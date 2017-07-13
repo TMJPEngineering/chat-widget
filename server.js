562cd9dd1d83f2f80dea821bef63d6e1c8b9552e
John Conrad Lumacad <jcmlumacad@tmj.ph>
2017-07-13 15:04:44 +0800

    added route get index

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ app.use('/bower_components', express.static(__dirname + '/bower_components'));
 app.use('/public', express.static(__dirname + '/public'));
 app.use('/views', express.static(__dirname + '/resources/views'));
 
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/resources/views/index.html')
+});
+
 app.listen(__env.NODE_PORT, function () {
     console.log('Listen localhost:' + __env.NODE_PORT);
 });
