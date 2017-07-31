62ce7ac428201af4646d5589a0c2dc6154bc5ff9
John Conrad Lumacad <jcmlumacad@tmj.ph>
2017-07-31 09:34:24 +0800

    changed icon

--- resources/assets/js/chat.js
+++ resources/assets/js/chat.js
@@ -1,25 +1,10 @@
+/* eslint no-shadow:*/
+
 var config = {
     link: "http://example.com"
 }
 
-initMiniChat(config);
-
-function initMiniChat(config) {
-    var head = document.head;
-    var externalCss = document.createElement('link');
-
-    externalCss.href = 'http://localhost:3000/public/css/chat.css';
-    externalCss.rel = 'stylesheet';
-
-    // Then bind the event to the activate function.
-    // There are several events for cross browser compatibility.
-    var callback = activate(config);
-
-    externalCss.onreadystatechange = callback;
-    externalCss.onload = callback;
-
-    head.appendChild(externalCss);
-}
+activate(config);
 
 function activate(config) {
     var miniChatContainer = document.createElement('div');
@@ -40,13 +25,13 @@ function activate(config) {
     var miniChatPaneClass = document.getElementsByClassName('mini-chat-pane');
     var miniChatMenuClass = document.getElementsByClassName('mini-chat-menu');
 
-    var lifeRingImage = "data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4KPHN2ZyB3aWR0aD0iMTc5MiIgaGVpZ2h0PSIxNzkyIiB2aWV3Qm94PSIwIDAgMTc5MiAxNzkyIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPjxwYXRoIGQ9Ik04OTYgMHExODIgMCAzNDggNzF0Mjg2IDE5MSAxOTEgMjg2IDcxIDM0OC03MSAzNDgtMTkxIDI4Ni0yODYgMTkxLTM0OCA3MS0zNDgtNzEtMjg2LTE5MS0xOTEtMjg2LTcxLTM0OCA3MS0zNDggMTkxLTI4NiAyODYtMTkxIDM0OC03MXptMCAxMjhxLTE5MCAwLTM2MSA5MGwxOTQgMTk0cTgyLTI4IDE2Ny0yOHQxNjcgMjhsMTk0LTE5NHEtMTcxLTkwLTM2MS05MHptLTY3OCAxMTI5bDE5NC0xOTRxLTI4LTgyLTI4LTE2N3QyOC0xNjdsLTE5NC0xOTRxLTkwIDE3MS05MCAzNjF0OTAgMzYxem02NzggNDA3cTE5MCAwIDM2MS05MGwtMTk0LTE5NHEtODIgMjgtMTY3IDI4dC0xNjctMjhsLTE5NCAxOTRxMTcxIDkwIDM2MSA5MHptMC0zODRxMTU5IDAgMjcxLjUtMTEyLjV0MTEyLjUtMjcxLjUtMTEyLjUtMjcxLjUtMjcxLjUtMTEyLjUtMjcxLjUgMTEyLjUtMTEyLjUgMjcxLjUgMTEyLjUgMjcxLjUgMjcxLjUgMTEyLjV6bTQ4NC0yMTdsMTk0IDE5NHE5MC0xNzEgOTAtMzYxdC05MC0zNjFsLTE5NCAxOTRxMjggODIgMjggMTY3dC0yOCAxNjd6IiBmaWxsPSIjZmZmIi8+PC9zdmc+";
+    var envelopeImage = "data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz48c3ZnIHdpZHRoPSIxNzkyIiBoZWlnaHQ9IjE3OTIiIHZpZXdCb3g9IjAgMCAxNzkyIDE3OTIiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZD0iTTE2NjQgMTUwNHYtNzY4cS0zMiAzNi02OSA2Ni0yNjggMjA2LTQyNiAzMzgtNTEgNDMtODMgNjd0LTg2LjUgNDguNS0xMDIuNSAyNC41aC0ycS00OCAwLTEwMi41LTI0LjV0LTg2LjUtNDguNS04My02N3EtMTU4LTEzMi00MjYtMzM4LTM3LTMwLTY5LTY2djc2OHEwIDEzIDkuNSAyMi41dDIyLjUgOS41aDE0NzJxMTMgMCAyMi41LTkuNXQ5LjUtMjIuNXptMC0xMDUxdi0yNC41bC0uNS0xMy0zLTEyLjUtNS41LTktOS03LjUtMTQtMi41aC0xNDcycS0xMyAwLTIyLjUgOS41dC05LjUgMjIuNXEwIDE2OCAxNDcgMjg0IDE5MyAxNTIgNDAxIDMxNyA2IDUgMzUgMjkuNXQ0NiAzNy41IDQ0LjUgMzEuNSA1MC41IDI3LjUgNDMgOWgycTIwIDAgNDMtOXQ1MC41LTI3LjUgNDQuNS0zMS41IDQ2LTM3LjUgMzUtMjkuNXEyMDgtMTY1IDQwMS0zMTcgNTQtNDMgMTAwLjUtMTE1LjV0NDYuNS0xMzEuNXptMTI4LTM3djEwODhxMCA2Ni00NyAxMTN0LTExMyA0N2gtMTQ3MnEtNjYgMC0xMTMtNDd0LTQ3LTExM3YtMTA4OHEwLTY2IDQ3LTExM3QxMTMtNDdoMTQ3MnE2NiAwIDExMyA0N3Q0NyAxMTN6IiBmaWxsPSIjZmZmIi8+PC9zdmc+";
     var timesImage = "data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4KPHN2ZyB3aWR0aD0iMTc5MiIgaGVpZ2h0PSIxNzkyIiB2aWV3Qm94PSIwIDAgMTc5MiAxNzkyIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPjxwYXRoIGQ9Ik0xNDkwIDEzMjJxMCA0MC0yOCA2OGwtMTM2IDEzNnEtMjggMjgtNjggMjh0LTY4LTI4bC0yOTQtMjk0LTI5NCAyOTRxLTI4IDI4LTY4IDI4dC02OC0yOGwtMTM2LTEzNnEtMjgtMjgtMjgtNjh0MjgtNjhsMjk0LTI5NC0yOTQtMjk0cS0yOC0yOC0yOC02OHQyOC02OGwxMzYtMTM2cTI4LTI4IDY4LTI4dDY4IDI4bDI5NCAyOTQgMjk0LTI5NHEyOC0yOCA2OC0yOHQ2OCAyOGwxMzYgMTM2cTI4IDI4IDI4IDY4dC0yOCA2OGwtMjk0IDI5NCAyOTQgMjk0cTI4IDI4IDI4IDY4eiIgZmlsbD0iI2ZmZiIvPjwvc3ZnPg==";
 
     document.body.appendChild(miniChatContainer);
 
     // Set life ring icon
-    miniChatMenuIconClass[0].setAttribute('src', lifeRingImage);
+    miniChatMenuIconClass[0].setAttribute('src', envelopeImage);
 
     document.getElementById('mini-chat-menu').addEventListener('click', function () {
         miniChatMenuIconClass[0].classList.toggle(timesClass);
@@ -57,7 +42,7 @@ function activate(config) {
         if (miniChatMenuIconClass[0].classList.contains(timesClass)) {
             miniChatMenuIconClass[0].setAttribute('src', timesImage);
         } else {
-            miniChatMenuIconClass[0].setAttribute('src', lifeRingImage);
+            miniChatMenuIconClass[0].setAttribute('src', envelopeImage);
         }
     });
 
